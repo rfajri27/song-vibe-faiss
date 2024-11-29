056d845b1c8b88285029550c44043173e9172fb6
rfajri27 <rfajri255@gmail.com>
2024-11-29 09:53:32 +0700

    update

--- modules/inference.py
+++ modules/inference.py
@@ -4,7 +4,7 @@ import numpy as np
 from modules.ui.utils import load_data, load_model, load_index
 
 
-def suggest(song_desc: str = ""):
+def suggest(song_desc: str = "", top_n=10):
     df = load_data()
     encoder = load_model()
     index = load_index()
@@ -16,7 +16,7 @@ def suggest(song_desc: str = ""):
     distances, ann = index.search(_vector, k=k)
 
     results = pd.DataFrame({"distances": distances[0], "ann": ann[0]})
-    results = results.head(10)
+    results = results.head(top_n)
     merge = pd.merge(results, df, left_on="ann", right_index=True)
-    res_dict = {"results": list(merge.to_dict()["category"].values())}
-    return res_dict
+    recom_df = merge[["category", "text"]]
+    return recom_df
